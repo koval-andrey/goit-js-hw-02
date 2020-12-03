4611ea4203a5bfa0e1be45e39f759c71b0950ba7
koval-andrey <kovalandrey1234@gmail.com>
2020-12-04 00:12:10 +0200

    04.12.2020
    ДЗ-2 4 задания

---
+++ js/task-2.js
@@ -0,0 +1,47 @@
+/*
+Напиши скрипт подсчета стоимости гравировки украшений. 
+Для этого создай функцию calculateEngravingPrice(message, pricePerWord) 
+принимающую строку (в строке будут только слова и пробелы) и цену гравировки одного слова, 
+и возвращающую цену гравировки всех слов в строке.
+
+const calculateEngravingPrice = function(message, pricePerWord) {
+  // твой код
+};
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+let pricePerWord = 10;
+let messanges = 'Proin sociis natoque et magnis parturient montes mus';
+
+const calculateEngravingPrice = function(messages, pricePerWord) {
+        
+    for(let message of messages) {
+     const totalPrice = messages.split(' ').length * pricePerWord;
+    console.log(totalPrice);
+    
+    return totalPrice;
+  }
+};
+console.log(
+    calculateEngravingPrice(
+      'Proin sociis natoque et magnis parturient montes mus',
+      10,
+    ),
+  );
+
+  console.log(
+    calculateEngravingPrice(
+      'Proin sociis natoque et magnis parturient montes mus',
+      20,
+    ),
+  );
+  console.log(
+    calculateEngravingPrice('Donec orci lectus aliquam est magnis', 40),
+  );
+  
+  console.log(
+    calculateEngravingPrice('Donec orci lectus aliquam est magnis', 20),
+  );
+
+
